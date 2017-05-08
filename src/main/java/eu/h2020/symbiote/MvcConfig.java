dcea9297c84eab311c24c67c9f5c3dcb2436a6a0
tipech <tipech@intracom-telecom.com>
2017-05-08 18:05:15 +0300

    Changed file structure to comply with Thymeleaf needs

--- src/main/java/eu/h2020/symbiote/MvcConfig.java
+++ src/main/java/eu/h2020/symbiote/MvcConfig.java
@@ -22,7 +22,6 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Bean
     public TemplateResolver templateResolver() {
         ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
-        templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode("HTML5");
         return templateResolver;
