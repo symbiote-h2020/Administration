d535b51f5e1bcc8816b184b2be0740e4215c8229
Vasilis Glykantzis <vasgl@intracom-telecom.com>
2017-09-02 09:53:27 +0300

    SYM-616 Added PlatformDetails class

---
+++ src/main/java/eu/h2020/symbiote/administration/model/Label.java
@@ -0,0 +1,24 @@
+package eu.h2020.symbiote.administration.model;
+
+import javax.validation.constraints.Size;
+
+/**
+ * Wrapper for validating comments
+ *
+ * @author Vasileios Glykantzis (ICOM)
+ */
+public class Label {
+
+    @Size(min=3, max=300)
+    private String label;
+
+    public Label() {
+    }
+
+    public Label(String label) {
+        setLabel(label);
+    }
+
+    public String getLabel() { return label; }
+    public void setLabel(String label) { this.label = label; }
+}
--- src/main/java/eu/h2020/symbiote/administration/model/PlatformDetails.java
+++ src/main/java/eu/h2020/symbiote/administration/model/PlatformDetails.java
@@ -1,29 +1,46 @@
 package eu.h2020.symbiote.administration.model;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import eu.h2020.symbiote.core.model.InterworkingService;
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+
+import java.util.List;
 
 
 /**
  * Class for a symbIoTe platform secondary details entity, used in form validation and messages
- * // TODO R3 more details e.g. location etc
  *
- * @author Tilemachos Pechlivanoglou (ICOM)
+ * @author Vasileios Glykantzis (ICOM)
  */
 public class PlatformDetails {
 
-    /* -------- Properties -------- */
+    @NotNull
+    @Pattern(regexp="(^\\Z|^[\\w-][\\w-][\\w-][\\w-]+\\Z)")
+    @Size(max=30)
+    private String id;
+
+    @NotNull
+    @Size(min=4, max=30)
+    private String name;
 
     @Size(min=3, max=300)
     private String description;
 
-    // @NotNull
-    @Size(min=3, max=30)
-    private String informationModelId;
+    @Valid
+    private List<String> labels;
 
+    @Valid
+    private List<String> comments;
 
-    /* -------- Constructors -------- */
+    // ToDo: Adding validation
+    private List<InterworkingService> interworkingServices;
+
+    @NotNull
+    private boolean enabler;
 
     /**
      * Empty constructor
@@ -32,39 +49,46 @@ public class PlatformDetails {
     }
 
     /**
-     * Constructor with properties
      *
-     * @param description           platform's description
-     * @param informationModelId    id of the platform's information model
+     * @param id                            platform id
+     * @param name                          platform name
+     * @param description                   platform description
+     * @param labels                        labels
+     * @param comments                      comments
+     * @param interworkingServices          list of interworking services
+     * @param enabler                       specify if it is an enabler
      */
-    public PlatformDetails(String description, String informationModelId) {
-
+    public PlatformDetails(String id, String name, String description, List<String> labels,
+                           List<String> comments, List<InterworkingService> interworkingServices, boolean enabler) {
+        this.id = id;
+        this.name = name;
         this.description = description;
-        this.informationModelId = informationModelId;
+        this.labels = labels;
+        this.comments = comments;
+        this.interworkingServices = interworkingServices;
+        this.enabler = enabler;
     }
 
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
 
-    /* -------- Getters & Setters -------- */
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public String getDescription() {
-        return this.description;
-    }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 
-    public String getInformationModelId() {
-        return this.informationModelId;
-    }
+    public List<String> getLabels() { return labels; }
+    public void setLabels(List<String> labels) { this.labels = labels; }
 
+    public List<String> getComments() { return comments; }
+    public void setComments(List<String> comments) { this.comments = comments; }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    public List<InterworkingService> getInterworkingServices() { return interworkingServices; }
+    public void setInterworkingServices(List<InterworkingService> interworkingServices) { this.interworkingServices = interworkingServices; }
 
-    public void setInformationModelId(String informationModelId) {
-        this.informationModelId = informationModelId;
-    }
-
-
-    /* -------- Helper Methods -------- */
+    public boolean isEnabler() { return enabler; }
+    public void setEnabler(boolean enabler) { this.enabler = enabler; }
 
     @Override
     public String toString() {
